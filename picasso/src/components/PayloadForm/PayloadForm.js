1bcd4d2cb0a011184f91c6b09b424a10844ee099
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-04-17 12:51:53 -0400

    Initializes toggle buttons for method choice

--- picasso/src/components/PayloadForm/PayloadForm.js
+++ picasso/src/components/PayloadForm/PayloadForm.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Form, FormGroup, Label, CustomInput, Button, Media, CardImg, CardColumns, Row } from 'reactstrap';
+import { Form, FormGroup, Label, CustomInput, ButtonGroup, Button, Media, CardImg, CardColumns, Row } from 'reactstrap';
 import { fetchStyleImages, baseURL, testURL, submitPayload } from '../../actions';
 import StyleImages from './StyleImages';
 import ResultImages from '../ResultImages';
@@ -14,12 +14,19 @@ class PayloadForm extends React.Component {
         styleID: null,
       },
       previewImg: null,
+      method: 'method1',
     }
   }
 
   componentDidMount() {
     !this.props.styleImages.length
       && this.props.fetchStyleImages();
+  }
+
+  handleMethodSelect = e => {
+    this.setState({
+      method: e.target.id,
+    })
   }
 
   handleUploadChange = e => {
@@ -83,10 +90,33 @@ class PayloadForm extends React.Component {
               <Button
                 type='submit'
                 disabled={noSubmit}
+                color={noSubmit ? 'secondary' : 'primary'}
               >
                 Submit
               </Button>
             </Row>
+          </FormGroup>
+          <FormGroup>
+            <Label style={{display: 'block'}} for="method-radio-btns">Pick a Method </Label>
+            <ButtonGroup id="method-radio-btns">
+              <Button
+                id='method1'
+                onClick={this.handleMethodSelect}
+                color='primary'
+                active={this.state.method === 'method1'}
+              >
+                Faster
+              </Button>
+              <Button
+                id='method2'
+                onClick={this.handleMethodSelect}
+                color={localStorage.getItem('token') ? 'primary' : 'secondary' }
+                disabled={!localStorage.getItem('token')}
+                active={this.state.method === 'method2'}
+              >
+                Slower
+              </Button>
+            </ButtonGroup>
           </FormGroup>
           <FormGroup>
             <Label for="upload-image">Pick an Image to Transform</Label>
@@ -109,10 +139,8 @@ class PayloadForm extends React.Component {
               </Media>
               <Media className='align-self-center'>
                 <Media body>
-                  <p>
-                    {this.state.formData.contentImg !== null
+                  {this.state.formData.contentImg !== null
                       && this.state.formData.contentImg.name}
-                  </p>
                 </Media>
               </Media>
             </Media>
