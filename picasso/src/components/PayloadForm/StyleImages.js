b1085bbcd3bfd30420139d9b0e945d3b8a7aeac4
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-04-17 09:24:43 -0400

    Moves radio buttons on style images, fixing bugs where buttons themselves wouldn't select & page would jump on successful selection

--- picasso/src/components/PayloadForm/StyleImages.js
+++ picasso/src/components/PayloadForm/StyleImages.js
@@ -8,8 +8,19 @@ export default props => {
         className='rounded-lg'
       >
         <Label className='StyleImage' style={{width: '100%'}} check>
+          <CardBody>
+            <CustomInput
+                type="radio"
+                checked={props.activeStyle === `${img.id}`}
+                onChange={props.handleStyleSelect}
+                id={img.id}
+                value={img.id}
+                name="style-select"
+                label="Select this style"
+              />
+          </CardBody>
           <CardImg
-            top
+            bottom
             id={img.id}
             width='100%'
             src={`${props.baseURL}/styles/${img.imageUrl}`}
@@ -22,15 +33,6 @@ export default props => {
             <p>
               <strong>Year(s):</strong> {img.year}
             </p>
-            <CustomInput
-              type="radio"
-              checked={props.activeStyle === `${img.id}`}
-              onChange={props.handleStyleSelect}
-              id={img.id}
-              value={img.id}
-              name="style-select"
-              label="Select this style"
-            />
           </CardBody>
         </Label>
       </Card>);
