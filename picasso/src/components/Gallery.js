9be6612a7b4832f659a85cd151ba470fe02a0b0f
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-04-17 22:34:12 -0400

    Initializes Gallery.js

--- picasso/src/components/Gallery.js
+++ picasso/src/components/Gallery.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { CardColumns, CardImg } from 'reactstrap';
 
 const Gallery = props => {
-  // return
+  return (
+    <CardColumns>
+      {this.props.gallery.map(img =>
+        <CardImg
+          key={img.request_key}
+          id={img.request_key}
+          className='rounded-lg'
+          src={img.output_url}
+          alt=''
+        />
+      )}
+    </CardColumns>
+  )
 }
 
 export default Gallery;
